f6dd70fa604ca9461f35e683a3bc3bd3a8624d4e
settes <ioanaroxana.st@gmail.com>
2026-04-18 20:53:36 +0200

    fix merge

--- index.ts
+++ index.ts
@@ -25,4 +25,5 @@ export {
   type InlineEditorSelectionSnapshot,
 } from './inlineEditorSelection';
 export { normalizeInlineLinkHref } from './inlineLinks';
+export { normalizeInlineSource } from './inlineSource';
 export { getCalloutIcon as getCalloutIconForKind } from './markdown/renderers/terminalHelpers';
